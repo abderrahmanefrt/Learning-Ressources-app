38c19cda9e394d50ab46fe5cb0e6efa63cd6d947
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2025-06-24 00:08:33 +0100

    first commit

--- src/components/learning-ressources/AddResource.vue
+++ src/components/learning-ressources/AddResource.vue
@@ -1,4 +1,57 @@
 <template>
   
-  <base-card></base-card>
+  <base-card>
+  <form action="">
+<div class="form-controler">
+<label for="">Title</label>
+    <input type="text" name="title" id="title">
+
+</div>
+<div class="form-controler">
+<label for="">Description</label>
+    <textarea type="text" name="title" id="description" rows="3"> </textarea>
+
+</div>
+<div class="form-controler">
+<label for="link">Link</label>
+    <input type="url" name="link" id="link">
+
+</div>
+<div>
+  <base-button type="submit">Add Ressource</base-button>
+</div>
+    
+    
+    
+  </form>
+  
+  </base-card>
 </template>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
